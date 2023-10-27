9924dbe30bfaef06157a5a50ca7781d704b473fd
Luka Kažić <kazicluka4@gmail.com>
2023-10-27 20:42:59 +0200

    Added better layout so user admin can see better what user bought, on professors demand

--- src/views/BoughtSneakersView.vue
+++ src/views/BoughtSneakersView.vue
@@ -2,18 +2,26 @@
     <div class="text-center">
         <p class="fw-bold h2 mb-3">Pregled kupljenih patika</p>
     </div>
-    <div class="wrapper mx-auto">
-        <div class="d-flex justify-content-center gap-1">
-            <div class="card col-3" v-for="sneaker in boughtSneakers" :key="sneaker.ID_PATIKA">
-                <img :src="sneaker.slika" class="card-img-top">
-                <div class="card-body">
-                    <h5 class="card-title">{{sneaker.brend}} {{sneaker.model}}</h5>
-                    <p class="card-text">Ukupno zaradjeno: {{sneaker.ukupnoZaradjeno}} RSD</p>
-                    <p class="card-text">Ukupno prodato: {{sneaker.ukupnoProdato}}</p>
-                </div>
-            </div>
+        <div class="table-wrapper">
+            <table>
+                <thead>
+                    <tr class="row100 head">
+                        <th class="cell100 column1">Brend</th>
+                        <th class="cell100 column2">Model</th>
+                        <th class="cell100 column3">Ukupno Prodato</th>
+                        <th class="cell100 column4">Ukupno Zaradjeno</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="sneaker in boughtSneakers" :key="sneaker.slika">
+                        <td>{{sneaker.brend}}</td>
+                        <td>{{sneaker.model}}</td>
+                        <td>{{sneaker.ukupnoProdato}} komada</td>
+                        <td>{{sneaker.ukupnoZaradjeno}} RSD</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-    </div>
 </template>
 
 <script setup>
@@ -29,7 +37,40 @@ const boughtSneakers = ref(null);
 </script>
 
 <style scoped>
-.wrapper {
-  max-width: 1250px;
+table {
+    border-collapse: collapse;
+    box-shadow: 0 5px 20px 0px rgba(0, 0, 0, 0.1);
+    width: 70%;
+    border-radius: 10px;
+}
+thead {
+    box-shadow: 0 5px 20px 0px rgba(0, 0, 0, 0.1);
+}
+tr {
+    text-align: center;
+    border-bottom: 1px solid #f2f2f2;
+    display: table;
+    width: 100%;
+    table-layout: fixed;
+}
+th {
+    color: crimson;
+    padding: 10px;
+}
+td {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    color: #808080;
+}
+.table-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+}
+tbody {
+    display: block;
+    max-height: 200px;
+    overflow: auto;
 }
 </style>
